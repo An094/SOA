78c12cee861b1aef0362568ee500ce4191caac62
An Ngo <ngothaian2409th@gmail.com>
2026-04-13 01:05:50 +0700

    Add Burst strategy

--- Assets/Scripts/SOA/PluggableBehavior/Strategies/BurstStrategy.cs
+++ Assets/Scripts/SOA/PluggableBehavior/Strategies/BurstStrategy.cs
@@ -1,10 +1,34 @@
+using System.Collections;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New BurstStrategy", menuName = "FireStrategies/BurstStrategy")]
 public class BurstStrategy : FireStrategy
 {
+    [SerializeField] private int _burstCount = 3;
+    [SerializeField] private float _burstDelay = 0.1f;
+
     public override void Fire(GameObject owner, Transform firePoint)
     {
-        throw new System.NotImplementedException();
+        if (owner.TryGetComponent(out MonoBehaviour monoBehaviour))
+        {
+            monoBehaviour.StartCoroutine(FireBurst(owner, firePoint));
+        }
+    }
+
+    private IEnumerator FireBurst(GameObject owner, Transform firePoint)
+    {
+        for (int i = 0; i < _burstCount; i++)
+        {
+            var projectileGO = Instantiate(_projectilePrefab, firePoint.position, firePoint.rotation);
+            if (projectileGO.TryGetComponent(out IProjectile projectile))
+            {
+                projectile.Initialize(owner, firePoint, firePoint.forward, _projectileSpeed, _projectileLifeTime);
+            }
+
+            if (i < _burstCount - 1)
+            {
+                yield return new WaitForSeconds(_burstDelay);
+            }
+        }
     }
 }
