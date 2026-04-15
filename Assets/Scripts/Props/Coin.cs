6fcf9beeff6a8cc0958ac98d5341b251f1d657a3
An Ngo <ngothaian2409th@gmail.com>
2026-04-16 01:16:25 +0700

    Add DoTween to make some animations

--- Assets/Scripts/Props/Coin.cs
+++ Assets/Scripts/Props/Coin.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class Coin : MonoBehaviour, ICollectable
@@ -5,10 +6,13 @@ public class Coin : MonoBehaviour, ICollectable
     [SerializeField] private bool _isInRoom;
     [SerializeField] private CollectableRuntimeSet _activeCoins;
     [SerializeField] private CollectableRuntimeSet _coinsInRoom;
+
+    private Tween _selfRotate;
+    private Tween _moveTo;
     private void OnEnable()
     {
-        _activeCoins.Add(this);
-        if(_isInRoom ) _coinsInRoom.Add(this);
+       _activeCoins.Add(this);
+       if(_isInRoom ) _coinsInRoom.Add(this);
     }
 
     private void OnDisable()
@@ -19,11 +23,18 @@ public class Coin : MonoBehaviour, ICollectable
 
     public void Collect(Transform targetTransform)
     {
-        Destroy(gameObject);
+        _selfRotate.Kill();
+        _moveTo = transform.DOJump(targetTransform.position, jumpPower: 3f, numJumps: 1, duration: 0.5f).OnComplete(() => { _moveTo.Kill(); Destroy(gameObject); });
+        
     }
 
     public bool CanCollect()
     {
         return true;
+    }
+
+    private void Start()
+    {
+        _selfRotate = transform.DORotate(new Vector3(0f, 360f, 0f), 2f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
     }
 }
